5c1a9af36a900ab0f4d10bb49eb58b2d5f2ddfe1
Akbota <aqbotamaratqyzy@gmail.com>
2024-05-30 21:15:58 +0500

    feat: env file

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,1 @@
-export const PORT = 5555;
 
-export const MongoDBURL='mongodb+srv://maratqyzyaqbota:0TRaZxpyL1l5AHjl@bookstore-mern.sikn74o.mongodb.net/?retryWrites=true&w=majority&appName=bookstore-MERN'
--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,29 @@
 import express from 'express';
-import {PORT, MongoDBURL} from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from "cors";
+import dotenv from 'dotenv'
 
 const app = express();
 
 app.use(express.json());
+
+dotenv.config();
 
 app.use(cors(
 ));
 
 app.use('/books', booksRoute);
 
+const port = process.env.PORT;
+const MongoDBURL = process.env.url;
+
 mongoose
     .connect(MongoDBURL)
     .then(()=>{
         console.log("Successfully connected to database!");
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
             }
         );
     })
